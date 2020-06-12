3ef870bfdbf3df4fbd1ab6e8c6b447131183d08d
Jon-Erik Nissen <jon-eriknissen@Jon-Eriks-MacBook-Pro.local>
2020-06-12 08:45:02 -0700

    second commit

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,63 +2,71 @@
 
 // Create a variable called 'myName' with a value that is your name. 
 
-// Code here
+var myName = "Jon-Erik";
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
-//Code here
+var faveNum = 666;
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and assign it a value of true.
 
-//Code here
+var lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
 
-//Code here
+let sum = (num1, num2) => num1 + num2;
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'. Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.  
 
-//Code here
+let lovesCodeChecker = x => {
+	if(x){
+		return true
+	}
+	else{
+		return false
+	}
+}
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, 'num'. Your function should check to see if the parameter is even or odd. If the parameter is odd, return 'the number is odd'. If the number is even, return 'the number is even'.
 
-//Code here
+let oddChecker = num => num%2===1;
+}
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'. Have the function take the two parameters and return a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
 
-//Code Here
+let iLove = (name, love) => `${name} loves ${love}`
 
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice.
 const faveColors = ['red', 'green', 'black']
 
-//Code Here
+let colorCopy = faveColors.slice(0,3);
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push.
 
-//Code Here
+colorCopy.push('aubergine');
 
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
-//Code Here
+let middleNums = numbers.slice(1,3);
 
 //////////////////PROBLEM 11////////////////////
 
